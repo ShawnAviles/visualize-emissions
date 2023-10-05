6ed0d2f0dfe98c4594bb4ed5ea3ab4f20d704e73
Harris Pyo <HPW9229@gmail.com>
2023-10-05 15:35:05 -0400

    added my last name

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,7 +3,7 @@ function Menu() {
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-3xl font-bold underline">Visualize Commuter Emissions</h1>
       <p>
-        By: Matteo, Bryce, Justin, Harris, & Shawn Aviles
+        By: Matteo, Bryce, Justin, Harris Pyo, & Shawn Aviles
       </p>
     </div>
   );
