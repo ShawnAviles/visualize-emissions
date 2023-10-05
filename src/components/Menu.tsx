3775e642fff3e290db2828a3fc9868fd0b7f38e4
juchen3637 <103432641+juchen3637@users.noreply.github.com>
2023-10-05 15:24:09 -0400

    'pk.eyJ1IjoibW1hcmNoZXNpIiwiYSI6ImNsaWl5NTd6aDAybmIzZnA2bjU0OXNxdWkifQ.KkUo_PUYfBodXqjokLNsZw' Changed names

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -3,7 +3,7 @@ function Menu() {
     <div className="flex flex-col justify-center items-center">
       <h1 className="text-3xl font-bold underline">Visualize Commuter Emissions</h1>
       <p>
-        By: Matteo, Bryce, Justin, Harris, & Shawn
+        By: Matteo, Bryce, Justin, Harris, & Shawn Aviles
       </p>
     </div>
   );
