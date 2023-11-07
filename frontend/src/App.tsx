f201ffffa9b8fbe0cd55443ccd385f38ae5f10db
Shawn <43704084+ShawnAviles@users.noreply.github.com>
2023-11-07 18:27:38 -0500

    Server implementation to get polyline routes from zipcode (#11)
    * server implementation to get polyline routes from zipcode
    
    * added env example

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,12 @@
 /* eslint-disable max-len */
 /* eslint-disable react/jsx-props-no-spreading */
-import Map, { Source, Layer, Marker } from 'react-map-gl';
 // import { useEffect, useState } from 'react';
 import { useState } from 'react';
 import Menu from './components/Menu';
-import ScopeMenu from './components/ScopeMenu';
+import MapWrapper from './components/MapWrapper';
 import routes from './components/Routes.json';
-import routeLayers from './components/RouteLayers.json';
 
 function App() {
-  const mapboxToken : string = import.meta.env.VITE_MAPBOX_TOKEN;
-  const StevensLongitude : number = -74.02414311907891;
-  const StevensLatitude : number = 40.74509007605575;
   const [routeNum, setRounteNum] = useState(0);
 
   /*
@@ -32,26 +27,14 @@ function App() {
     <>
       <Menu />
       {/* {create two button to toggle between routes plus and minus} */}
-      <button type="button" className="w-32 flex justify-center bg-blue-100 p-3 mb-4 rounded-lg" onClick={() => setRounteNum((routeNum + 1) % routes.length)}>Toggle Route</button>
-      <Map
-        mapboxAccessToken={mapboxToken}
-        initialViewState={{
-          longitude: StevensLongitude,
-          latitude: StevensLatitude,
-          zoom: 10,
-        }}
-        style={{ width: '100vw', height: '80vh'}}
-        mapStyle="mapbox://styles/mapbox/streets-v12"
+      <button 
+        type="button" 
+        className="w-32 flex justify-center bg-blue-100 p-3 mb-4 rounded-lg" 
+        onClick={() => setRounteNum((routeNum + 1) % routes.length)}
       >
-        <ScopeMenu />
-        <Marker longitude={StevensLongitude} latitude={StevensLatitude} color="#b30538" anchor="bottom" />
-        <Source id="my-data1" type="geojson" data={routes[routeNum]}>
-          <Layer {...routeLayers[routeNum]} />
-        </Source>
-        {/* <Source id="my-data2" type="geojson" data={routes[1]}>
-          <Layer {...routeLayers[1]} />
-        </Source> */}
-      </Map>
+        Toggle Route
+      </button>
+      <MapWrapper routeNum={routeNum}/>
     </>
   );
 }
