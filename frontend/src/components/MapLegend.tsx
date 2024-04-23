e1cede234a007869581e9c6e1e5181fa2ccf658e
juchen3637 <103432641+juchen3637@users.noreply.github.com>
2024-04-23 18:01:22 -0400

    changes to centering

--- frontend/src/components/MapLegend.tsx
+++ frontend/src/components/MapLegend.tsx
@@ -8,14 +8,14 @@ const MapLegend: React.FC<LegendProps> = ({ modeColors }) => {
   return (
     <div className="flex flex-col mt-3">
       <div className="font-bold text-md mb-2">Legend</div>
-      <div className="flex flex-wrap">
+      <div className="flex flex-wrap justify-center">
         {Object.entries(modeColors).map(([mode, color]) => (
           <div key={mode} className="flex items-center mb-2 mr-4">
             <div
               className="w-4 h-4 mr-1 rounded-full"
               style={{ backgroundColor: color }}
             ></div>
-            <div className="md:text-base text-[10px]">{mode}</div>
+            <div>{mode}</div>
           </div>
         ))}
       </div>
